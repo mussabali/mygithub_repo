2cf7738fd9ca2b2553ef6fcb320ecaf3c6d876d3
mussabali <musabhusien@gmail.com>
2022-10-09 14:13:15 +0200

    my local commit

--- helloword.c
+++ helloword.c
@@ -7,5 +7,6 @@ void main()
 printf("======= hello world =========");
 printf("I hope you are fine");
   printf("I adding new brach from them main = add enhancement");
+  printf("clone to my PC and would like to push to my github repo");
   
 }
